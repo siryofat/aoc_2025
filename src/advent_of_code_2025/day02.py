16b72acefebfbe0c07779ce38c07c62a6807676c
sirYofat <siryofat@gmail.com>
2025-12-03 07:20:16 +0100

    feat: day02 part02 solved

--- src/advent_of_code_2025/day02.py
+++ src/advent_of_code_2025/day02.py
@@ -6,14 +6,21 @@ with open("./inputs/day02.txt") as file:
 ranges = line.split(",")
 
 pat = re.compile(r"^(.+)\1$")
+pat_02 = re.compile(r"^(.+)\1+$")
 total = 0
+total_pat02 = 0
 
 for rang in ranges:
     first, second = rang.split("-")
     for num in range(int(first), int(second) + 1):
         num_str = str(num)
         found = pat.search(num_str)
+        f_02 = pat_02.search(num_str)
         if found:
             total += num
+        if f_02:
+            total_pat02 += num
+
 
 print(total)
+print(total_pat02)
