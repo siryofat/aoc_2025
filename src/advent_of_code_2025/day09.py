156d817f4dcccfe1087ac64be01225c9ab0474a5
sirYofat <siryofat@gmail.com>
2025-12-10 06:19:24 +0100

    feat: solve day09 part02

--- src/advent_of_code_2025/day09.py
+++ src/advent_of_code_2025/day09.py
@@ -1,6 +1,8 @@
 from collections.abc import Generator
 from pathlib import Path
 
+from shapely import LineString, Polygon
+
 cwd = Path(__file__).parent
 file = cwd / "inputs/day09.txt"
 
@@ -27,4 +29,43 @@ def day09_01(file):
     return max_area
 
 
+def line_in_polygon(pol: Polygon, line: LineString):
+    return pol.covers(line)
+
+
+def day09_02(file):
+    points = []
+    max_rect = 0
+    max_p1 = ()
+    max_p2 = ()
+    for point in extract_points(file):
+        points.append(point)
+    points.append(points[0])
+    polygon = Polygon(points)
+    for i, p1 in enumerate(points):
+        for p2 in points[i + 1 :]:
+            x1, y1 = p1
+            x2, y2 = p2
+            side1 = LineString([(x1, y1), (x2, y1)])
+            side2 = LineString([(x2, y1), (x2, y2)])
+            side3 = LineString([(x2, y2), (x1, y2)])
+            side4 = LineString([(x1, y2), (x1, y1)])
+            if not (
+                line_in_polygon(polygon, side1)
+                and line_in_polygon(polygon, side2)
+                and line_in_polygon(polygon, side3)
+                and line_in_polygon(polygon, side4)
+            ):
+                continue
+            d1 = abs(x1 - x2) + 1
+            d2 = abs(y1 - y2) + 1
+            area = d1 * d2
+            max_rect = area if area > max_rect else max_rect
+            if area >= max_rect:
+                max_p1 = p1
+                max_p2 = p2
+    return max_rect, max_p1, max_p2
+
+
 print(day09_01(file))
+print(day09_02(file))
