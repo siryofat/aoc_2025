afef3bcec07f07261d81c6654d8970a561f3057d
sirYofat <siryofat@gmail.com>
2025-12-17 06:48:48 +0100

    feat: solve day07 part 02

--- src/advent_of_code_2025/day07.py
+++ src/advent_of_code_2025/day07.py
@@ -1,4 +1,5 @@
 from collections.abc import Generator
+from functools import lru_cache
 from pathlib import Path
 
 cwd = Path(__file__).parent
@@ -37,4 +38,38 @@ def day07_01(file):
     return splits
 
 
-print(day07_01(file))
+def day07_02(file):
+    lines = list(extract_lines(file))
+    start = lines[0].index("S")
+    rows = len(lines)
+    cols = len(lines[0])
+
+    @lru_cache(maxsize=None)
+    def count_splits(r: int, c: int) -> int:
+        nr = r + 1
+
+        # base:
+        if nr > rows:
+            return 0
+
+        if not (0 <= nr < rows and 0 <= c < cols):
+            return 0
+
+        pos = lines[nr][c]
+
+        if pos == "^":
+            total = 1
+
+            left = count_splits(nr, c - 1)
+            right = count_splits(nr, c + 1)
+
+            return total + left + right
+        else:
+            return count_splits(nr, c)
+
+    result = 1 + count_splits(0, start)
+    return result
+
+
+# print(day07_01(file))
+print(day07_02(file))
