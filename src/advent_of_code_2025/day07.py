f36a13e446473d61e8807cd495e2b124bfc3cc56
sirYofat <siryofat@gmail.com>
2025-12-17 06:31:15 +0100

    feat: day07 part 01 in its function

--- src/advent_of_code_2025/day07.py
+++ src/advent_of_code_2025/day07.py
@@ -11,27 +11,30 @@ def extract_lines(file_path) -> Generator[str]:
             yield line.strip()
 
 
-generator = extract_lines(file)
-start_line = next(generator)
-max_index = len(start_line)
-beam_index = {start_line.index("S")}
-print(beam_index)
+def day07_01(file):
+    generator = extract_lines(file)
+    start_line = next(generator)
+    max_index = len(start_line)
+    beam_index = {start_line.index("S")}
 
-splits = 0
+    splits = 0
 
-for line in generator:
-    line_splitters = set()
-    for i, char in enumerate(line):
-        if char == "^":
-            line_splitters.add(i)
-    splitters = line_splitters & beam_index
-    splits += len(splitters)
-    new_beams = set()
-    for split in splitters:
-        left = max(0, split - 1)
-        right = min(max_index, split + 1)
-        new_beams.update({left, right})
-    beam_index -= splitters
-    beam_index |= new_beams
+    for line in generator:
+        line_splitters = set()
+        for i, char in enumerate(line):
+            if char == "^":
+                line_splitters.add(i)
+        splitters = line_splitters & beam_index
+        splits += len(splitters)
+        new_beams = set()
+        for split in splitters:
+            left = max(0, split - 1)
+            right = min(max_index, split + 1)
+            new_beams.update({left, right})
+        beam_index -= splitters
+        beam_index |= new_beams
 
-print(f"{beam_index} with {len(beam_index)=}, split {splits} times.")
+    return splits
+
+
+print(day07_01(file))
